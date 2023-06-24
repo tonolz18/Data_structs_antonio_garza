ae998868ddcd4975ceb9598f4dad57f784cee7b1
tonolz18 <antoniogz_lozano@outlook.com>
2023-06-24 11:54:12 -0600

    Subiendo archivos

---
+++ main/Deque.h
@@ -0,0 +1,22 @@
+#ifndef DEQUE_H
+#define DEQUE_H
+
+template <class T>
+class Deque {
+private:
+    T deque[MAX_SIZE];
+    int head;
+    int tail;
+
+public:
+    Deque();
+    bool isFull();
+    bool isEmpty();
+    void FrontEnqueue(T element);
+    void BackDequeue();
+    T Front();
+    T Back();
+    void Print();
+};
+
+#endif
