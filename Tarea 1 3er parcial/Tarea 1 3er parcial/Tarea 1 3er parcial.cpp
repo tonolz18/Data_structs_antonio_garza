1b06a6216f2651757cf990a80f39928d8d083870
tonolz18 <antoniogz_lozano@outlook.com>
2023-07-30 16:38:30 -0600

    Subiendo archivos

--- Tarea 1 3er parcial/Tarea 1 3er parcial/Tarea 1 3er parcial.cpp
+++ Tarea 1 3er parcial/Tarea 1 3er parcial/Tarea 1 3er parcial.cpp
@@ -7,51 +7,52 @@ template<typename T>
 class BinaryTree
 {
 private:
+    // Estructura de Nodo para representar cada elemento en el árbol binario
     template <typename U>
-    struct Node
+    struct Nodo
     {
     public:
-        Node(U in_data, Node<U>* in_parent, Node<U>* in_left = nullptr, Node<U>* in_right = nullptr) :
+        Nodo(U in_data, Nodo<U>* in_parent, Nodo<U>* in_left = nullptr, Nodo<U>* in_right = nullptr) :
             data(in_data), parent(in_parent), left(in_left), right(in_right)
         {
         }
 
-        ~Node()
+        ~Nodo()
         {
         }
 
         U data;
-        Node<U>* parent;
-        Node<U>* left;
-        Node<U>* right;
+        Nodo<U>* parent;
+        Nodo<U>* left;
+        Nodo<U>* right;
     };
 
-    Node<T>* root; 
+    Nodo<T>* root; // Puntero a la raíz del árbol binario
 
 public:
     BinaryTree() : root(nullptr)
     {
     }
 
-
+    // Destructor para liberar la memoria ocupada por el árbol
     ~BinaryTree()
     {
         while (root != nullptr)
         {
-            DeleteNode(root);
+            EliminarNodo(root);
         }
     }
 
-    // Función que devuelve el puntero al nodo raíz del árbol.
-    Node<T>* GetRoot()
+    // Función que devuelve un puntero al nodo raíz del árbol
+    Nodo<T>* ObtenerRaiz()
     {
         return root;
     }
 
-    // Función auxiliar para encontrar el nodo con el valor mínimo a partir de un nodo dado.
-    Node<T>* MinimumFromNode(Node<T>* in_root)
+    // Función para encontrar el nodo con el valor mínimo a partir de un nodo dado
+    Nodo<T>* MinimoDesdeNodo(Nodo<T>* in_root)
     {
-        Node<T>* temp = in_root;
+        Nodo<T>* temp = in_root;
         while (temp->left != nullptr)
         {
             temp = temp->left;
@@ -59,26 +60,27 @@ public:
         return temp;
     }
 
-    // Función que devuelve el nodo con el valor mínimo en el árbol.
-    Node<T>* Minimum()
+    // Función que devuelve el nodo con el valor mínimo en el árbol
+    Nodo<T>* Minimo()
     {
-        return MinimumFromNode(root);
+        return MinimoDesdeNodo(root);
     }
 
-    // Función que devuelve el valor mínimo en el árbol.
-    T MinimumValue()
+    // Función que devuelve el valor mínimo en el árbol
+    T ValorMinimo()
     {
-        Node<T>* temp = Minimum();
+        Nodo<T>* temp = Minimo();
         if (temp != nullptr)
         {
             return temp->data;
         }
+        // En caso de que el árbol esté vacío, se debe manejar este caso también (por ejemplo, lanzando una excepción o devolviendo un valor predeterminado)
     }
 
-    // Función auxiliar para encontrar el nodo con el valor máximo a partir de un nodo dado.
-    Node<T>* MaximumFromNode(Node<T>* in_root)
+    // Función para encontrar el nodo con el valor máximo a partir de un nodo dado
+    Nodo<T>* MaximoDesdeNodo(Nodo<T>* in_root)
     {
-        Node<T>* temp = in_root;
+        Nodo<T>* temp = in_root;
         while (temp->right != nullptr)
         {
             temp = temp->right;
@@ -86,29 +88,30 @@ public:
         return temp;
     }
 
-    // Función que devuelve el nodo con el valor máximo en el árbol.
-    Node<T>* Maximum()
+    // Función que devuelve el nodo con el valor máximo en el árbol
+    Nodo<T>* Maximo()
     {
-        return MaximumFromNode(root);
+        return MaximoDesdeNodo(root);
     }
 
-    // Función que devuelve el valor máximo en el árbol.
-    T MaximumValue()
+    // Función que devuelve el valor máximo en el árbol
+    T ValorMaximo()
     {
-        Node<T>* temp = Maximum();
+        Nodo<T>* temp = Maximo();
         if (temp != nullptr)
         {
             return temp->data;
         }
+        // En caso de que el árbol esté vacío, se debe manejar este caso también (por ejemplo, lanzando una excepción o devolviendo un valor predeterminado)
     }
 
-    // Función que busca un valor en el árbol y devuelve el nodo que lo contiene (si existe).
-    Node<T>* Search(T value)
+    // Función para buscar un valor en el árbol y devolver el nodo que lo contiene (si existe)
+    Nodo<T>* Buscar(T valor)
     {
-        Node<T>* temp = root;
-        while (temp != nullptr && temp->data != value)
+        Nodo<T>* temp = root;
+        while (temp != nullptr && temp->data != valor)
         {
-            if (value < temp->data)
+            if (valor < temp->data)
             {
                 temp = temp->left;
             }
@@ -120,115 +123,115 @@ public:
         return temp;
     }
 
-    // Función para insertar un nuevo valor en el árbol binario.
-    void Insert(T value)
+    // Función para insertar un nuevo valor en el árbol binario
+    void Insertar(T valor)
     {
-        Node<T>* temp = root;
-        Node<T>* temp_parent = nullptr;
+        Nodo<T>* temp = root;
+        Nodo<T>* temp_padre = nullptr;
         while (temp != nullptr)
         {
-            if (value < temp->data)
+            if (valor < temp->data)
             {
-                temp_parent = temp;
+                temp_padre = temp;
                 temp = temp->left;
             }
             else
             {
-                temp_parent = temp;
+                temp_padre = temp;
                 temp = temp->right;
             }
         }
 
-        Node<T>* newNode = new Node<T>(value, temp_parent);
-        if (temp_parent == nullptr)
+        Nodo<T>* nuevoNodo = new Nodo<T>(valor, temp_padre);
+        if (temp_padre == nullptr)
         {
-            root = newNode;
+            root = nuevoNodo;
         }
-        else if (value < temp_parent->data)
+        else if (valor < temp_padre->data)
         {
-            temp_parent->left = newNode;
+            temp_padre->left = nuevoNodo;
         }
         else
         {
-            temp_parent->right = newNode;
+            temp_padre->right = nuevoNodo;
         }
     }
 
-    // Función que devuelve el sucesor de un nodo dado en el árbol.
-    Node<T>* Sucesor(Node<T>* in_x)
+    // Función que devuelve el sucesor de un nodo dado en el árbol
+    Nodo<T>* Sucesor(Nodo<T>* in_x)
     {
         if (in_x->right != nullptr)
         {
-            return MinimumFromNode(in_x->right);
+            return MinimoDesdeNodo(in_x->right);
         }
         else
         {
-            Node<T>* current = in_x;
-            Node<T>* temp_parent = in_x->parent;
-            while (temp_parent != nullptr && temp_parent->right == current)
+            Nodo<T>* actual = in_x;
+            Nodo<T>* temp_padre = in_x->parent;
+            while (temp_padre != nullptr && temp_padre->right == actual)
             {
-                current = temp_parent;
-                temp_parent = current->parent;
+                actual = temp_padre;
+                temp_padre = actual->parent;
             }
-            return temp_parent;
+            return temp_padre;
         }
     }
 
-    // Función que devuelve el predecesor de un nodo dado en el árbol.
-    Node<T>* Predecesor(Node<T>* in_x)
+    // Función que devuelve el predecesor de un nodo dado en el árbol
+    Nodo<T>* Predecesor(Nodo<T>* in_x)
     {
         if (in_x->left != nullptr)
         {
-            return MaximumFromNode(in_x->left);
+            return MaximoDesdeNodo(in_x->left);
         }
         else
         {
-            Node<T>* current = in_x;
-            Node<T>* temp_parent = in_x->parent;
-            while (temp_parent != nullptr && temp_parent->left == current)
+            Nodo<T>* actual = in_x;
+            Nodo<T>* temp_padre = in_x->parent;
+            while (temp_padre != nullptr && temp_padre->left == actual)
             {
-                current = temp_parent;
-                temp_parent = current->parent;
+                actual = temp_padre;
+                temp_padre = actual->parent;
             }
-            return temp_parent;
+            return temp_padre;
         }
     }
 
-    // Función para recorrer el árbol en orden (in-order) y mostrar los valores.
-    void InOrder(Node<T>* in_x)
+    // Función para recorrer el árbol en orden (in-order) y mostrar los valores
+    void EnOrden(Nodo<T>* in_x)
     {
         if (in_x != nullptr)
         {
-            InOrder(in_x->left);
+            EnOrden(in_x->left);
             std::cout << in_x->data << ", ";
-            InOrder(in_x->right);
+            EnOrden(in_x->right);
         }
     }
 
-    // Función para recorrer el árbol en preorden (pre-order) y mostrar los valores.
-    void PreOrder(Node<T>* in_x)
+    // Función para recorrer el árbol en preorden (pre-order) y mostrar los valores
+    void PreOrden(Nodo<T>* in_x)
     {
         if (in_x != nullptr)
         {
             std::cout << in_x->data << ", ";
-            PreOrder(in_x->left);
-            PreOrder(in_x->right);
+            PreOrden(in_x->left);
+            PreOrden(in_x->right);
         }
     }
 
-    // Función para recorrer el árbol en postorden (post-order) y mostrar los valores.
-    void PostOrder(Node<T>* in_x)
+    // Función para recorrer el árbol en postorden (post-order) y mostrar los valores
+    void PostOrden(Nodo<T>* in_x)
     {
         if (in_x != nullptr)
         {
-            PostOrder(in_x->left);
-            PostOrder(in_x->right);
+            PostOrden(in_x->left);
+            PostOrden(in_x->right);
             std::cout << in_x->data << ", ";
         }
     }
 
-    // Función auxiliar para reemplazar un nodo del árbol con otro nodo.
-    void Transplant(Node<T>* in_u, Node<T>* in_v)
+    // Función para reemplazar un nodo del árbol con otro nodo
+    void Transplant(Nodo<T>* in_u, Nodo<T>* in_v)
     {
         if (in_u->parent == nullptr)
         {
@@ -248,8 +251,8 @@ public:
         }
     }
 
-    // Función para eliminar un nodo del árbol.
-    void DeleteNode(Node<T>* in_z)
+    // Función para eliminar un nodo del árbol
+    void EliminarNodo(Nodo<T>* in_z)
     {
         if (in_z->left == nullptr)
         {
@@ -261,7 +264,7 @@ public:
         }
         else
         {
-            Node<T>* Y = MinimumFromNode(in_z->right);
+            Nodo<T>* Y = MinimoDesdeNodo(in_z->right);
             if (Y != in_z->right)
             {
                 Transplant(Y, Y->right);
@@ -275,100 +278,99 @@ public:
         delete in_z;
     }
 
-    // Función para realizar el recorrido in-order de manera iterativa.
-    void InOrderIterative()
+    // Función para realizar el recorrido in-order de manera iterativa
+    void EnOrdenIterativo()
     {
         if (root == nullptr)
         {
             return;
         }
 
-        std::stack<Node<T>*> nodeStack;
-        Node<T>* current = root;
+        std::stack<Nodo<T>*> pilaNodos;
+        Nodo<T>* actual = root;
 
-        while (current != nullptr || !nodeStack.empty())
+        while (actual != nullptr || !pilaNodos.empty())
         {
-            while (current != nullptr)
+            while (actual != nullptr)
             {
-                nodeStack.push(current);
-                current = current->left;
+                pilaNodos.push(actual);
+                actual = actual->left;
             }
 
-            current = nodeStack.top();
-            nodeStack.pop();
+            actual = pilaNodos.top();
+            pilaNodos.pop();
 
-            std::cout << current->data << ", ";
+            std::cout << actual->data << ", ";
 
-            current = current->right;
+            actual = actual->right;
         }
     }
 
-    // Función para realizar el recorrido pre-order de manera iterativa.
-    void PreOrderIterative()
+    // Función para realizar el recorrido pre-order de manera iterativa
+    void PreOrdenIterativo()
     {
         if (root == nullptr)
         {
             return;
         }
 
-        std::stack<Node<T>*> nodeStack;
-        nodeStack.push(root);
+        std::stack<Nodo<T>*> pilaNodos;
+        pilaNodos.push(root);
 
-        while (!nodeStack.empty())
+        while (!pilaNodos.empty())
         {
-            Node<T>* current = nodeStack.top();
-            nodeStack.pop();
+            Nodo<T>* actual = pilaNodos.top();
+            pilaNodos.pop();
 
-            std::cout << current->data << ", ";
+            std::cout << actual->data << ", ";
 
-            if (current->right != nullptr)
+            if (actual->right != nullptr)
             {
-                nodeStack.push(current->right);
+                pilaNodos.push(actual->right);
             }
 
-            if (current->left != nullptr)
+            if (actual->left != nullptr)
             {
-                nodeStack.push(current->left);
+                pilaNodos.push(actual->left);
             }
         }
     }
 
-    // Función para realizar el recorrido post-order de manera iterativa.
-    void PostOrderIterative()
+    // Función para realizar el recorrido post-order de manera iterativa
+    void PostOrdenIterativo()
     {
         if (root == nullptr)
         {
             return;
         }
 
-        std::stack<Node<T>*> stack1, stack2;
-        stack1.push(root);
+        std::stack<Nodo<T>*> pila1, pila2;
+        pila1.push(root);
 
-        // Llenar stack2 con el recorrido post-order invertido.
-        while (!stack1.empty())
+        // Llenar pila2 con el recorrido post-order invertido
+        while (!pila1.empty())
         {
-            Node<T>* current = stack1.top();
-            stack1.pop();
-            stack2.push(current);
+            Nodo<T>* actual = pila1.top();
+            pila1.pop();
+            pila2.push(actual);
 
-            if (current->left != nullptr)
+            if (actual->left != nullptr)
             {
-                stack1.push(current->left);
+                pila1.push(actual->left);
             }
 
-            if (current->right != nullptr)
+            if (actual->right != nullptr)
             {
-                stack1.push(current->right);
+                pila1.push(actual->right);
             }
         }
 
-        // Mostrar los valores del recorrido post-order.
-        while (!stack2.empty())
+        // Mostrar los valores del recorrido post-order
+        while (!pila2.empty())
         {
-            Node<T>* current = stack2.top();
-            stack2.pop();
-            std::cout << current->data << ", ";
+            Nodo<T>* actual = pila2.top();
+            pila2.pop();
+            std::cout << actual->data << ", ";
         }
     }
 };
-
