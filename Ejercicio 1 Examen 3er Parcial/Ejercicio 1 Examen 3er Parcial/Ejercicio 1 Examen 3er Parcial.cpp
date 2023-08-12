5381aa0fc4fbe5d36287085c3383ed90e23e83a4
tonolz18 <antoniogz_lozano@outlook.com>
2023-08-12 11:45:26 -0600

    Subiendo archivos

--- Ejercicio 1 Examen 3er Parcial/Ejercicio 1 Examen 3er Parcial/Ejercicio 1 Examen 3er Parcial.cpp
+++ Ejercicio 1 Examen 3er Parcial/Ejercicio 1 Examen 3er Parcial/Ejercicio 1 Examen 3er Parcial.cpp
@@ -1,19 +1,33 @@
-// Ejercicio 1 Examen 3er Parcial.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// Definición de la clase RawArray con un tamaño de arreglo fijo.
+template<size_t Size>
+class RawArray {
+private:
+    int elements[Size]; // Arreglo para almacenar los elementos.
+
+public:
+    // Constructor que inicializa el arreglo con valores del 1 al Size.
+    RawArray() {
+        for (size_t i = 0; i < Size; ++i) {
+            elements[i] = i + 1;
+        }
+    }
+
+    // Función para acceder a los elementos con índices negativos.
+    int& MyFunction(int index) {
+        if (index >= 0) {
+            return elements[index]; // Acceso a elementos con índice positivo.
+        }
+        else {
+            return elements[Size + index]; // Acceso a elementos con índice negativo.
+        }
+    }
+};
+
+int main() {
+    RawArray<5> myArray; // Creación de un objeto RawArray con un tamaño de 5.
+    std::cout << myArray.MyFunction(-1) << std::endl; // Imprimirá '5', accediendo al último elemento.
+
+    return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
